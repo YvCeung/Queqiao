face270ee20babc2f90abb2ae16dc29933f99680
xiaoyu <zytuatua@163.com>
2025-07-18 11:33:32 +0800

    feature:增加固定分割符的方式解决半包黏包的case

---
+++ basic/src/main/java/org/xiaoyu/basic/pack/resolution/LineSplitorServer.java
@@ -0,0 +1,67 @@
+package org.xiaoyu.basic.pack.resolution;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 固定分隔符的方式解决半包黏包
+ */
+@Slf4j
+public class LineSplitorServer {
+    public static void main(String[] args) {
+        new LineSplitorServer().start();
+    }
+
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    // 超过这个长度如果还没找到分割符 就会抛出异常
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            log.debug("connected {}", ctx.channel());
+                            super.channelActive(ctx);
+                        }
+
+                        @Override
+                        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                            log.debug("disconnect {}", ctx.channel());
+                            super.channelInactive(ctx);
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(9090);
+            log.debug("{} binding...", channelFuture.channel());
+            channelFuture.sync();
+            log.debug("{} bound...", channelFuture.channel());
+            // Netty 自己内部不会阻塞，但我们需要让 main 主线程阻塞住，以防应用提前退出。
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.debug("stoped");
+        }
+    }
+}
